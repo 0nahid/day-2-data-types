94320f45cbe7943e48e9e9b97c3eb28abe43c569
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-29 01:11:43 +0600

    added two digits addition

---
+++ type.py
@@ -0,0 +1,16 @@
+# num_char = len(input("Enter a string: "))
+# new_num_char=str(num_char)
+# print("The number of characters in the string is: " + new_num_char)
+a=str(123);
+# print(type(a))
+# print(70+ float(10.5))
+# print(int(10.5))
+# print(float(10.5))
+# print(int(10.5)+float(10.5))
+
+two_digit_num = input("Enter a two digit number: ")
+first_digit = two_digit_num[0]
+second_digit = two_digit_num[1]
+print("The first digit is: " + first_digit + " and the second digit is: " + second_digit)
+result = int(first_digit) + int(second_digit)
+print("The sum of the two digits is: " + str(result))
