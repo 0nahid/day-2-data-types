faf2c42f2260e7f6b1340131cc66c015d3e03136
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-29 22:54:07 +0600

    added bmi checker

---
+++ bmi.py
@@ -0,0 +1,4 @@
+# weight = input("Enter your weight in kilograms: ")
+# height = input("Enter your height in meters: ")
+# print("Your BMI is: ", int(float(weight) / (float(height) ** 2)))
+print("Your BMI is: ", int(float(input("Enter your weight in kilograms: ")) / (float(input("Enter your height in meters: ")) ** 2)));
