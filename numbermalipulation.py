2b684ef09ae80b854bf0e5a1e895ba8c9325cca9
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-30 00:35:54 +0600

    added number mulipulation

---
+++ numbermalipulation.py
@@ -0,0 +1,15 @@
+result = 4/2;
+result /= 2;
+score = 100;
+height = 1.78;
+isWorking = True;
+
+# print("your score is {score}".format(score=score) , "your height is {height}".format(height=height) , "is working?: {isWorking}".format(isWorking=isWorking));
+
+# print("{0} {1} {2} {3} ".format(result, score, height,isWorking));
+
+# print("{0:.2f} {1:.2f} {2:.2f} {3:.2f} ".format(result, score, height,isWorking));
+
+print(" He is number {0} in the class , he always score {1} in the exam , his height is {2} m and is he work hard ? ans :  {3}\r\n".format(result, score, height,isWorking));
+
+print(f"your score is {score} & you are {int(result)} in the exam & his height is {height} & he always work hard? ans:{isWorking} ")
